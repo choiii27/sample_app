d19eae98064d6a1c5afc3d3491a3b22762524710
Phil Choi <philchoiii@gmail.com>
2011-11-29 05:48:26 -0500

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"     #assignment @ indicates instance variable
   end
 
   def contact
+    @title = "Contact"  #assignment
+  end
+  
+  def about
+    @title = "About"    #assignment
   end
 
 end
